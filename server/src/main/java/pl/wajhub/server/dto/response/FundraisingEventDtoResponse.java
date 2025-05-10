011183b83d9b0d687d6b84a09836fafe99f09cd1
Hubert Wajda <hubert.wajda03@gmail.com>
2025-05-10 17:43:43 +0200

    update description in swagger about currencyCode

--- server/src/main/java/pl/wajhub/server/dto/response/FundraisingEventDtoResponse.java
+++ server/src/main/java/pl/wajhub/server/dto/response/FundraisingEventDtoResponse.java
@@ -1,9 +1,16 @@
 package pl.wajhub.server.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
+import pl.wajhub.server.validation.CurrencyCodeConstraint;
 
 import java.util.UUID;
 
 @Builder
-public record FundraisingEventDtoResponse(UUID uuid, String name, String currencyCode, Double amount) {
+public record FundraisingEventDtoResponse(
+        UUID uuid,
+        String name,
+        @Schema(name = "CurrencyCode", example = "PLN", description = "Currency code according to ISO 4217")
+        @CurrencyCodeConstraint String currencyCode,
+        Double amount) {
 }
